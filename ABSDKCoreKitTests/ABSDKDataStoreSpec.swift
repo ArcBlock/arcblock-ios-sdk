826cb8a4c5c90222b5b921a86bffda9e35f36418
jonathanlu813 <jonathanlu813@gmail.com>
2018-05-08 09:34:06 -0700

    fix test failure on travis

--- ABSDKCoreKitTests/ABSDKDataStoreSpec.swift
+++ ABSDKCoreKitTests/ABSDKDataStoreSpec.swift
@@ -10,124 +10,127 @@ import Nimble
 
 class ABSDKDataStoreSpec: QuickSpec {
     override func spec() {
-        describe("a data store") {
-            var datastore : ABSDKDataStore!
-            let registeredCollection = "registeredCollection"
-            let nonregisteredCollection = "nonregisteredCollection"
-            let key1 = "key1"
-            let key2 = "key2"
-            let value1 = "value1"
-            let value2 = "value2"
-            var observers = [NSKeyValueObservation]()
+        var datastore : ABSDKDataStore!
+        let registeredCollection = "registeredCollection"
+        let nonregisteredCollection = "nonregisteredCollection"
+        let key1 = "key1"
+        let key2 = "key2"
+        let value1 = "value1"
+        let value2 = "value2"
+        var observers = [NSKeyValueObservation]()
+        
+        beforeSuite {
+            datastore = ABSDKDataStore.sharedInstance()
+            datastore.registerCollections([registeredCollection])
+            datastore.setupDataStore(nil);
+        }
+        
+        describe("CRUD with registered collection", {
+            var hasChange:Bool?
             
             beforeSuite {
-                datastore = ABSDKDataStore.sharedInstance()
-                datastore.registerCollections([registeredCollection])
+                NotificationCenter.default.addObserver(forName: Notification.Name.ABSDKDataStoreModified, object: nil, queue: nil, using: { (notification) in
+                    hasChange = datastore.hasChange(forKey: key1, inCollection: registeredCollection, notification: notification)
+                })
             }
             
-            describe("data store lifecycle", {
-                var dataStoreReadyChanged:Bool?
-                
-                beforeEach {
-                    observers.append(datastore.observe(\.dataStoreReady, changeHandler: { (datastore, changed) in
-                        dataStoreReadyChanged = true
-                    }))
-                }
-                
-                it("setup") {
-                    dataStoreReadyChanged = false
-                    datastore.setupDataStore(nil)
-                    expect(datastore.dataStoreReady).to(beTrue())
-                    expect(dataStoreReadyChanged).toEventually(beTrue())
-                }
-                
-                it("quit") {
-                    dataStoreReadyChanged = false
-                    datastore.quitDataStore()
-                    expect(datastore.dataStoreReady).to(beFalse())
-                    expect(dataStoreReadyChanged).toEventually(beTrue())
-                }
+            beforeEach {
+                hasChange = false
+            }
+            
+            it("create", closure: {
+                datastore.setObject(value1, forKey: key1, inCollection: registeredCollection, completionBlock: nil)
+                expect((datastore.object(forKey: key1, inCollection: registeredCollection) as? String)).toEventually(equal(value1))
+                expect(hasChange).toEventually(beTrue())
             })
             
-            describe("setup", {
-                
+            it("update with same value", closure: {
+                datastore.setObject((datastore.object(forKey: key1, inCollection: registeredCollection) as? String), forKey: key1, inCollection: registeredCollection, completionBlock: nil)
+                expect(hasChange).toEventually(beFalse())
             })
             
-            describe("CRUD with registered collection", {
-                var hasChange:Bool?
-                beforeEach {
-                    datastore.setupDataStore(nil)
-                    hasChange = false
-                    NotificationCenter.default.addObserver(forName: Notification.Name.ABSDKDataStoreModified, object: nil, queue: nil, using: { (notification) in
-                        hasChange = datastore.hasChange(forKey: key1, inCollection: registeredCollection, notification: notification)
-                    })
-                }
-                
-                it("create", closure: {
-                    datastore.setObject(value1, forKey: key1, inCollection: registeredCollection, completionBlock: nil)
-                    expect((datastore.object(forKey: key1, inCollection: registeredCollection) as? String)).toEventually(equal(value1))
-                    expect(hasChange).toEventually(beTrue())
-                })
-                
-                it("update with same value", closure: {
-                    datastore.setObject((datastore.object(forKey: key1, inCollection: registeredCollection) as? String), forKey: key1, inCollection: registeredCollection, completionBlock: nil)
-                    expect(hasChange).toEventually(beFalse())
-                })
-                
-                it("update with different value", closure: {
-                    datastore.setObject(value2, forKey: key1, inCollection: registeredCollection, completionBlock: nil)
-                    expect((datastore.object(forKey: key1, inCollection: registeredCollection) as? String)).toEventually(equal(value2))
-                    expect(hasChange).toEventually(beTrue())
-                })
-                
-                it("remove", closure: {
-                    datastore.removeObject(forKey: key1, inCollection: registeredCollection, completionBlock: nil)
-                    expect((datastore.object(forKey: key1, inCollection: registeredCollection) as? String)).toEventually(beNil())
-                    expect(hasChange).toEventually(beTrue())
-                })
+            it("update with different value", closure: {
+                datastore.setObject(value2, forKey: key1, inCollection: registeredCollection, completionBlock: nil)
+                expect((datastore.object(forKey: key1, inCollection: registeredCollection) as? String)).toEventually(equal(value2))
+                expect(hasChange).toEventually(beTrue())
             })
             
-            describe("CRUD with nonregistered collection", {
-                var hasChange:Bool?
-                beforeEach {
-                    datastore.setupDataStore(nil)
-                    hasChange = false
-                    NotificationCenter.default.addObserver(forName: Notification.Name.ABSDKDataStoreModified, object: nil, queue: nil, using: { (notification) in
-                        hasChange = datastore.hasChange(forKey: key2, inCollection: nonregisteredCollection, notification: notification)
-                    })
-                }
-                
-                it("create") {
-                    datastore.setObject(value2, forKey: key2, inCollection: nonregisteredCollection, completionBlock: nil)
-                    expect((datastore.object(forKey: key2, inCollection: nonregisteredCollection) as? String)).toEventually(equal(value2))
-                    expect(hasChange).toEventually(beTrue())
-                }
-                
-                it("update with same value", closure: {
-                    datastore.setObject((datastore.object(forKey: key2, inCollection: nonregisteredCollection) as? String), forKey: key2, inCollection: nonregisteredCollection, completionBlock: nil)
-                    expect(hasChange).toEventually(beFalse())
+            it("remove", closure: {
+                datastore.removeObject(forKey: key1, inCollection: registeredCollection, completionBlock: nil)
+                expect((datastore.object(forKey: key1, inCollection: registeredCollection) as? String)).toEventually(beNil())
+                expect(hasChange).toEventually(beTrue())
+            })
+        })
+        
+        describe("CRUD with nonregistered collection", {
+            var hasChange:Bool?
+            
+            beforeSuite {
+                NotificationCenter.default.addObserver(forName: Notification.Name.ABSDKDataStoreModified, object: nil, queue: nil, using: { (notification) in
+                    hasChange = datastore.hasChange(forKey: key2, inCollection: nonregisteredCollection, notification: notification)
                 })
-                
-                it("update with different value", closure: {
-                    datastore.setObject(value1, forKey: key2, inCollection: nonregisteredCollection, completionBlock: nil)
-                    expect((datastore.object(forKey: key2, inCollection: nonregisteredCollection) as? String)).toEventually(equal(value1))
-                    expect(hasChange).toEventually(beTrue())
-                })
-                
-                it("remove", closure: {
-                    datastore.removeObject(forKey: key2, inCollection: nonregisteredCollection, completionBlock: nil)
-                    expect((datastore.object(forKey: key2, inCollection: nonregisteredCollection) as? String)).toEventually(beNil())
-                    expect(hasChange).toEventually(beTrue())
-                })
+            }
+            
+            beforeEach {
+                hasChange = false
+            }
+            
+            it("create") {
+                datastore.setObject(value2, forKey: key2, inCollection: nonregisteredCollection, completionBlock: nil)
+                expect((datastore.object(forKey: key2, inCollection: nonregisteredCollection) as? String)).toEventually(equal(value2))
+                expect(hasChange).toEventually(beTrue())
+            }
+            
+            it("update with same value", closure: {
+                datastore.setObject((datastore.object(forKey: key2, inCollection: nonregisteredCollection) as? String), forKey: key2, inCollection: nonregisteredCollection, completionBlock: nil)
+                expect(hasChange).toEventually(beFalse())
             })
             
-            afterSuite {
+            it("update with different value", closure: {
+                datastore.setObject(value1, forKey: key2, inCollection: nonregisteredCollection, completionBlock: nil)
+                expect((datastore.object(forKey: key2, inCollection: nonregisteredCollection) as? String)).toEventually(equal(value1))
+                expect(hasChange).toEventually(beTrue())
+            })
+            
+            it("remove", closure: {
+                datastore.removeObject(forKey: key2, inCollection: nonregisteredCollection, completionBlock: nil)
+                expect((datastore.object(forKey: key2, inCollection: nonregisteredCollection) as? String)).toEventually(beNil())
+                expect(hasChange).toEventually(beTrue())
+            })
+        })
+        
+        describe("lifecycle", {
+            var dataStoreReadyChanged:Bool?
+
+            beforeSuite {
+                observers.append(datastore.observe(\.dataStoreReady, changeHandler: { (datastore, changed) in
+                    dataStoreReadyChanged = true
+                }))
+            }
+            
+            beforeEach {
+                dataStoreReadyChanged = false
+            }
+
+            it("setup") {
+                datastore.setupDataStore(nil)
+                expect(datastore.dataStoreReady).to(beTrue())
+                expect(dataStoreReadyChanged).toEventually(beTrue())
+            }
+
+            it("quit") {
                 datastore.quitDataStore()
-                datastore = nil
-                let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
-                let dbFilePath = paths[0].appending("/tmp.sqlite")
-                try? FileManager.default.removeItem(atPath: dbFilePath)
+                expect(datastore.dataStoreReady).to(beFalse())
+                expect(dataStoreReadyChanged).toEventually(beTrue())
             }
+        })
+        
+        afterSuite {
+            datastore.quitDataStore()
+            datastore = nil
+            let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+            let dbFilePath = paths[0].appending("/tmp.sqlite")
+            try? FileManager.default.removeItem(atPath: dbFilePath)
         }
     }
 }
