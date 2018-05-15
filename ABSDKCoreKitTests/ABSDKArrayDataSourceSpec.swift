b8ff20a317dda4fd934a84cb4a61960940e20037
jonathanlu813 <jonathanlu813@gmail.com>
2018-05-15 10:09:21 -0700

    add update cell and remove cell test case

--- ABSDKCoreKitTests/ABSDKArrayDataSourceSpec.swift
+++ ABSDKCoreKitTests/ABSDKArrayDataSourceSpec.swift
@@ -9,40 +9,32 @@ import Quick
 import Nimble
 
 class ABSDKArrayDataSourceSpec: QuickSpec {
-    enum Collection: String {
-        case tableView, collectionView
-    }
 
     var datastore: ABSDKDataStore!
     var tableView: UITableView!
-    var collectionView: UICollectionView!
     var tableViewArrayDataSource: ABSDKArrayDataSource!
-    var collectionViewArrayDataSource: ABSDKArrayDataSource!
     let tableViewCellIdentifier = "tableViewCellIdentifier"
-    let collectionViewCellIdentifier = "collectionViewCellIdentifier"
+    let collection = "arrayDataSourceCollection"
     let key = "key"
     var tableViewObjects: [[String: Any]] = []
-    var collectionViewObjects: [[String: Any]] = []
 
     override func spec() {
 
         beforeSuite {
             self.datastore = ABSDKDataStore.sharedInstance()
-            self.datastore.registerCollections([Collection.tableView.rawValue, Collection.collectionView.rawValue])
+            self.datastore.registerCollections([self.collection])
 
             self.tableView = UITableView.init(frame: CGRect(x: 0, y: 0, width: 320, height: 640))
             self.tableView.dataSource = self
-            self.collectionView = UICollectionView.init(frame: CGRect(x: 0, y: 0, width: 320, height: 640), collectionViewLayout: UICollectionViewLayout.init())
-            self.collectionView.dataSource = self
 
             let sorting: ABSDKArrayDataSourceSortingBlock = { (_, _, key1, _, _, key2, _) -> ComparisonResult in
                 return (key1?.compare(key2!))!
             }
 
-            self.tableViewArrayDataSource = ABSDKArrayDataSource.init(identifier: "tableViewArrayDataSource",
-                                                                 sections: [Collection.tableView.rawValue],
+            self.tableViewArrayDataSource = ABSDKArrayDataSource.init(identifier: self.tableViewCellIdentifier,
+                                                                 sections: [self.collection],
                                                                  grouping: { (collection, _, _) -> String? in
-                                                                        if collection == Collection.tableView.rawValue {
+                                                                        if collection == self.collection {
                                                                             return collection
                                                                         }
                                                                         return nil
@@ -51,43 +43,48 @@ class ABSDKArrayDataSourceSpec: QuickSpec {
             self.tableView.observe(self.tableViewArrayDataSource, updatedBlock: nil)
             self.tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: self.tableViewCellIdentifier)
 
-            self.collectionViewArrayDataSource = ABSDKArrayDataSource.init(identifier: "collectionViewArrayDataSource",
-                                                                      sections: [Collection.collectionView.rawValue],
-                                                                      grouping: { (collection, _, _) -> String? in
-                                                                            if collection == Collection.collectionView.rawValue {
-                                                                                return collection
-                                                                            }
-                                                                            return nil
-                                                                        },
-                                                                      sorting: sorting)
-            self.collectionView.observe(self.collectionViewArrayDataSource, updatedBlock: nil)
-            self.collectionView.register(UICollectionViewCell.classForCoder(), forCellWithReuseIdentifier: self.collectionViewCellIdentifier)
-
             for index in 1...2 {
                 self.tableViewObjects.append(["_id": self.key, "name": String(index)])
             }
-
-            for index in 1...2 {
-                self.collectionViewObjects.append(["_id": self.key, "name": String(index)])
-            }
         }
 
-        describe("insert row") {
+        describe("insert row in table view") {
             beforeEach {
                 self.datastore.setupDataStore(nil)
+                self.datastore.setObject(self.tableViewObjects[0], forKey: self.key, inCollection: self.collection, completionBlock: nil)
             }
 
-            context("in table view", {
-                beforeEach {
-                    self.datastore.setObject(self.tableViewObjects[0], forKey: self.key, inCollection: Collection.tableView.rawValue, completionBlock: nil)
-                }
+            it("cell inserted", closure: {
+                expect(self.tableView.numberOfSections).toEventually(equal(1))
+                expect(self.tableView.numberOfRows(inSection: 0)).toEventually(equal(1))
+                let text = self.tableViewObjects[0]["name"] as? String
+                expect(self.tableView.cellForRow(at: IndexPath(row: 0, section: 0))?.textLabel?.text).toEventually(equal(text))
+            })
+        }
 
-                it("cell inserted", closure: {
-                    expect(self.tableView.numberOfSections).toEventually(equal(1))
-                    expect(self.tableView.numberOfRows(inSection: 0)).toEventually(equal(1))
-                    let text = self.tableViewObjects[0]["name"] as? String
-                    expect(self.tableView.cellForRow(at: IndexPath(row: 0, section: 0))?.textLabel?.text).toEventually(equal(text))
-                })
+        describe("update row in table view") {
+            beforeEach {
+                self.datastore.setupDataStore(nil)
+                self.datastore.setObject(self.tableViewObjects[1], forKey: self.key, inCollection: self.collection, completionBlock: nil)
+            }
+
+            it("cell updated", closure: {
+                expect(self.tableView.numberOfSections).toEventually(equal(1))
+                expect(self.tableView.numberOfRows(inSection: 0)).toEventually(equal(1))
+                let text = self.tableViewObjects[1]["name"] as? String
+                expect(self.tableView.cellForRow(at: IndexPath(row: 0, section: 0))?.textLabel?.text).toEventually(equal(text))
+            })
+        }
+
+        describe("remove row in table view") {
+            beforeEach {
+                self.datastore.setupDataStore(nil)
+                self.datastore.removeObject(forKey: self.key, inCollection: self.collection, completionBlock: nil)
+            }
+
+            it("cell deleted", closure: {
+                expect(self.tableView.numberOfSections).toEventually(equal(1))
+                expect(self.tableView.numberOfRows(inSection: 0)).toEventually(equal(0))
             })
         }
     }
@@ -105,21 +102,6 @@ extension ABSDKArrayDataSourceSpec: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: tableViewCellIdentifier, for: indexPath)
         cell.textLabel?.text = self.tableViewArrayDataSource.object(at: indexPath)["name"] as? String
-        return cell
-    }
-}
-
-extension ABSDKArrayDataSourceSpec: UICollectionViewDataSource {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return self.collectionViewArrayDataSource.numberOfSections()
-    }
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.collectionViewArrayDataSource.numberOfItems(forSection: section)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionViewCellIdentifier, for: indexPath)
         return cell
     }
 }
