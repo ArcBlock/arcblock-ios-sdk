33c59fe8ee9eb684a5c3907362433177078fe462
jonathanlu813 <jonathanlu813@gmail.com>
2018-05-16 14:55:13 -0700

    fix bump_podspec_version.sh

--- .makefiles/bump_podspec_version.sh
+++ .makefiles/bump_podspec_version.sh
@@ -3,4 +3,4 @@
 cur_ver=$(cat ArcBlockSDK.podspec | grep s.version |  head -1 | awk '{print $3}' | sed "s/'//g")
 new_ver=$(cat version)
 cat ArcBlockSDK.podspec | sed "s/"${cur_ver}"/${new_ver}/g" >  ArcBlockSDK.podspec.tmp
-# mv ArcBlockSDK.podspec.tmp ArcBlockSDK.podspec
+mv ArcBlockSDK.podspec.tmp ArcBlockSDK.podspec
