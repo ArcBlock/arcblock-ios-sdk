53557f67b97cee25c898a6eb470892585f82ad61
jonathanlu813 <jonathanlu813@gmail.com>
2018-06-13 14:17:20 +0800

    run jazzy in root directory

--- ABSDKMessagingKit/ABSDKMessagingKit.h
+++ ABSDKMessagingKit/ABSDKMessagingKit.h
@@ -20,12 +20,3 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for ABSDKMessagingKit.
-FOUNDATION_EXPORT double ABSDKMessagingKitVersionNumber;
-
-//! Project version string for ABSDKMessagingKit.
-FOUNDATION_EXPORT const unsigned char ABSDKMessagingKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ABSDKMessagingKit/PublicHeader.h>
