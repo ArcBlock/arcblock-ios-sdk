e13c283bba37ff36d6f5c8431c99ddce678d9618
jonathanlu813 <jonathanlu813@gmail.com>
2018-08-20 17:33:27 +0800

    add paged view controller file template

---
+++ Templates/ArcBlock/Paged Query View Controller.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,28 @@
+//___FILEHEADER___
+
+import UIKit
+import ArcBlockSDK
+
+class BlockListViewController: ABSDKTableViewController<___VARIABLE_Query___, ___VARIABLE_Data___, ___VARIABLE_Cell___> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func configDataSource() {
+        // config the parameters for initiating data source
+
+        /*
+        client = ...
+
+        dataSourceMapper = { (data) in
+            return ...
+        }
+        pageMapper = { (data) in
+            return ...
+        }
+        query = ...
+        */
+    }
+}
