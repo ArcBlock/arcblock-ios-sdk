5d9c68287cac32c55ecd803d5f19f9b96a486bae
jonathanlu813 <jonathanlu813@gmail.com>
2018-11-16 21:25:13 +0800

    support function input

--- ABSDKCoreKit/ABSDKCustomScalarTypes.swift
+++ ABSDKCoreKit/ABSDKCustomScalarTypes.swift
@@ -5,8 +5,19 @@
 //  Created by Jonathan Lu on 21/9/2018.
 //
 
-import Foundation
+import Apollo
 
 public typealias BigNumber = String
 
 public typealias DateTime = String
+
+public typealias FunctionInput = [String: Any]
+
+extension Dictionary: JSONDecodable where Key == String, Value == Any {
+    public init(jsonValue value: JSONValue) throws {
+        guard let dict = value as? [String: Any] else {
+            throw JSONDecodingError.couldNotConvert(value: value, to: [String: Any].self)
+        }
+        self = dict
+    }
+}
