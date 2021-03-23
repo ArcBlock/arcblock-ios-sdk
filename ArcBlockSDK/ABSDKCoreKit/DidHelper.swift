cbbbbe72f438257de7e22884d50ccd170b4d988c
karthus <675377091@qq.com>
2021-03-23 20:29:11 +0800

    revert last change

--- ArcBlockSDK/ABSDKCoreKit/DidHelper.swift
+++ ArcBlockSDK/ABSDKCoreKit/DidHelper.swift
@@ -386,10 +386,7 @@ public class DidHelper {
         return DidType(roleType: roleType, keyType: keyType, hashType: hashType, encodingType: encodingType)
     }
     
-    public static func removeDidPrefix(_ did: String?) -> String {
-        guard let did = did else {
-            return ""
-        }
+    public static func removeDidPrefix(_ did: String) -> String {
         if did.hasPrefix("did:abt:") {
             var newDid = did
             newDid.removeFirst(8)
