584daffc42f0456685d114fce24b7b078021fdb8
karthus <675377091@qq.com>
2022-05-06 22:12:08 -0700

    add fastlane

--- ArcBlockSDKTests/DidHelperSpec.swift
+++ ArcBlockSDKTests/DidHelperSpec.swift
@@ -35,6 +35,7 @@ class DidHelperSpec: QuickSpec {
             })
         }
 
+        // TODO: - 
 //        describe("seed to did") {
 //            it("works", closure: {
 //                let seed = Data.init(hex: "07abfceff5cdfb0cd164d2da98099c15b7223fc5a1b8c02c2cf1f74670c72aac27e1d28ed47cf4f2c4330a6e6e1dc0724721e80fa56177fdba926937a253fe7e")
