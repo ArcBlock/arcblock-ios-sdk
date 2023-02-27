0b48cf59c365f77dc90f58822dbdd405c27cee2a
DavidYu <654070281@qq.com>
2023-02-27 15:38:49 +0800

    fix encrypt define

--- ArcBlockSDK/ABSDKCoreKit/AESUtils.swift
+++ ArcBlockSDK/ABSDKCoreKit/AESUtils.swift
@@ -73,6 +73,13 @@ public class AESUtils {
         }
         return String(bytes: bytes, encoding: .utf8)
     }
+    /// 解密String到String
+    public static func decryptString2String(_ string: String, key: String) -> String? {
+        guard let bytes = Data(multibaseEncoded: string)?.bytes, let decrypted = decryptByte2Byte(bytes, key: key) else {
+            return nil
+        }
+        return String(bytes: decrypted, encoding: .utf8)
+    }
     /// 解密hex到Byte
     public static func decryptHex2Byte(_ hex: String, key: String) -> Array<UInt8>? {
         decryptByte2Byte(Data(hex: hex).bytes, key: key)
@@ -81,12 +88,12 @@ public class AESUtils {
     public static func decryptHex2String(_ hex: String, key: String) -> String? {
         decryptByte2String(Data(hex: hex).bytes, key: key)
     }
-    /// 解密base46到Byte
+    /// 解密base64到Byte
     public static func decryptBase642Byte(_ base64: String, key: String) -> Array<UInt8>? {
         guard let bytes = Data(base64Encoded: base64)?.bytes else { return nil }
         return decryptByte2Byte(bytes, key: key)
     }
-    /// 解密base46到String
+    /// 解密base64到String
     public static func decryptBase642String(_ base64: String, key: String) -> String? {
         guard let bytes = Data(base64Encoded: base64)?.bytes else { return nil }
         return decryptByte2String(bytes, key: key)
